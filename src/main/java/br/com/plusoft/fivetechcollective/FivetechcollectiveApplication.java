00799a8db26e10643318a46223623f0d38e68e25
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-06-16 16:57:33 -0300

    upload dos arquivos base do projeto 5tech para as sprints 3 e 4 do Challenge Plusoft

---
+++ src/main/java/br/com/plusoft/fivetechcollective/FivetechcollectiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.plusoft.fivetechcollective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivetechcollectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivetechcollectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/plusoft/fivetechcollective/FivetechcollectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plusoft.fivetechcollective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivetechcollectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
